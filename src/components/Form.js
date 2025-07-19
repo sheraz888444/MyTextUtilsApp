288d54ced061ff776683ca539a61bc43bf152458
muhammad saqib <saqib.s09651100@gmail.com>
2025-07-19 23:34:04 +0500

    Add components folder with Navbar, Form, About

---
+++ src/components/Form.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { Link }  from 'react-router-dom'
+export function Form(props) {
+  const [text, setText] = useState('');
+
+  const handleUpperCase = () => {
+    setText(text.toUpperCase());
+  };
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+const handlelowerCase = () => {
+    setText(text.toLowerCase());
+  };
+  const handleclear = () => {
+    let newText=" ";
+    setText(newText);
+  };
+   const handlecopy = () => {
+    navigator.clipboard.writeText(text);
+    alert("Text Copied to clipboard");
+    
+  };
+
+
+
+  
+  return (
+    <>
+    <div className="container my-4">
+      <h2 className="mb-3">{props.heading}</h2>
+
+      <div className="mb-3">
+        <textarea
+          className="form-control"
+          rows="8"
+          placeholder=" "
+          value={text}
+          onChange={handleChange}
+        ></textarea>
+      </div>
+<button className="btn btn-primary mx-1" onClick={handleUpperCase}>
+       Uppercase
+      </button>
+
+      <button className="btn btn-primary mx-1" onClick={handlelowerCase}>
+       LowerCase
+     </button>
+      <button className="btn btn-primary mx-1" onClick={handleclear}>
+      Clear 
+     </button>
+      <button className="btn btn-primary mx-1" onClick={handlecopy}>
+      Copy 
+     </button>
+<div className='container'>
+    <h2>Text Summery </h2>
+    <p>{text.split(' ').length} words</p>
+    <p>{text.length} characters</p>
+     <p>{0.008 * text.split(' ').length} Minuites to read</p>
+
+
+</div>
+     
+    </div>
+    </>
+    
+  );
+}
+
+export default Form;
