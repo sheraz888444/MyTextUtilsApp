4bc19558113076157eeafac7c43f07665046589e
muhammad saqib <saqib.s09651100@gmail.com>
2025-07-20 06:38:31 +0500

    initialized new project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
+import Navbar from './components/Navbar';
+import Form from './components/Form';
+import About from './components/About';
+
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { useState } from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [mode,setmode]=useState('light');
+const toggleMode=()=>{
+  if(mode==='light'){
+setmode('dark');
+document.body.style.backgroundColor='#042743'
+document.body.style.color='white'
+document.title='Textutils App -Dark Mode'
+
+    }  else{
+setmode('light');
+document.body.style.backgroundColor='white';
+document.body.style.color='black'
+document.title='Textutils App -Light Mode'
+
+
+
+  }
 }
 
+
+
+  return (
+   <>
+  
+  
+  
+  
+  <Router>
+      <Navbar title="my app" mode={mode} toggleMode={toggleMode} /> 
+      
+      <Routes>     {/* switch is exchange with routes in new version of react router */}
+        <Route path="/" element={<Form heading="Enter a Text To Analyze:"/>} />     {/* Default page  */}
+         <Route path="/About" element={<About />} />  
+    
+      </Routes> 
+    </Router>
+ </>
+  );
+
+}
+
+
 export default App;
+ //<button className="btn btn-primary" onClick={handleUpperCase}>
+    //    Convert to Uppercase
+   //   </button>
+
+    //  <button className="btn btn-primary" onClick={handlelowerCase}>
+     //   Convert to LowerCase
+     // </button>
